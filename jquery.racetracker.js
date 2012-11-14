697c75c28ba098189ef8e1f10fc1dff641cefb17
4kategreen <kate@kategreen.us>
2012-11-14 15:58:22 -0500

    Adding basic graphing tools

--- jquery.racetracker.js
+++ jquery.racetracker.js
@@ -4,20 +4,47 @@
     normalizeDistance = '3.1' // 1, 3.1, 6.2, 13.1, 26.2 (in miles for now)
     formula = true, // some other day i'll add more formulae
     distanceType = 'miles', // miles, km
+    dataSource = 'form', // form, csv, json
+    dataLocation = '.', // if it's form, blank, but otherwise location in tree?
+    graphId = 'graph'
   };
+
+  var data = [];
 
   var methods = {
     init: function(options) { 
       
     },
     uploadData: function( ) {
-      // IS
+      // get data source
+
+      // get data
+      var precheckData = '';
+
+      // validate
+      this.validateData();
+
+      // save to data 
+      precheckData.each(function() {
+        data.push(this.);
+      });
     },
     validateData: function( ) { 
       // GOOD
     },
     graph: function(content) { 
-      // !!! 
+      $.jqplot(graphId,[data], {
+        axes: {
+          xaxis: {
+            label: 'Race Dates',
+            renderer: $.jqplot.DateAxisRenderer,
+            numberTicks: 4,
+            tickOptions: {
+              formatString:'%b&nbsp;%#d',
+            }
+          }
+        }
+      })
     },
     list: function(content) {
 
