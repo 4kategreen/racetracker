64a372a31c31035358f7510e6d02587718c29bcc
4kategreen <kate@kategreen.us>
2012-11-14 14:21:33 -0500

    adding settings

--- jquery.racetracker.js
+++ jquery.racetracker.js
@@ -1,22 +1,34 @@
 (function( $ ){
+  var settings = {
+    normalize = true, // normalize the values to one distance
+    normalizeDistance = '3.1' // 1, 3.1, 6.2, 13.1, 26.2 (in miles for now)
+    formula = true, // some other day i'll add more formulae
+    distanceType = 'miles', // miles, km
+  };
 
   var methods = {
-    init : function( options ) { 
-      // THIS 
+    init: function(options) { 
+      
     },
-    show : function( ) {
+    uploadData: function( ) {
       // IS
     },
-    hide : function( ) { 
+    validateData: function( ) { 
       // GOOD
     },
-    update : function( content ) { 
+    graph: function(content) { 
       // !!! 
+    },
+    list: function(content) {
+
+    },
+    clear: function() {
+
     }
   };
 
-  $.fn.racetracker = function( method ) {
-    
+  $.fn.racetracker = function(method) {
+
     // Method calling logic
     if ( methods[method] ) {
       return methods[ method ].apply( this, Array.prototype.slice.call( arguments, 1 ));
