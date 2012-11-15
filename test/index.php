e9e549fb67441d3aa8efe44a38768a5d1720091a
4kategreen <kate@kategreen.us>
2012-11-15 13:43:32 -0500

    adding qunit testing fun. just infrastructure now.

---
+++ test/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>racetracker tests</title>
+  <link rel="stylesheet" href="qunit.css">
+  <script src="../../jquery.jqplot.min.js"></script>
+	<script src="../../jqplot.dateAxisRenderer.min.js"></script>
+	<script src="../../jqplot.cursor.min.js"></script>
+	<script src="../../jqplot.highlighter.min.js"></script>
+	<script src="../jquery.racetracker.js"></script>
+	<script src="../../race_tracker.js"></script>
+</head>
+<body>
+  <div id="qunit"></div>
+  <div id="qunit-fixture"></div>
+  <script src="qunit.js"></script>
+  <script src="tests.js"></script>
+</body>
+</html>
